0959d790a613efa9633e977da9ef7b88f7a14e4d
ali <alialdawoodi99@yahoo.com>
2024-01-31 20:10:41 -0700

    Filtered out according data

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
     require 'net/http'
     require 'json'
 
-    @url = 'https://api.open-meteo.com/v1/forecast?latitude=39.739235&longitude=-104.990250&current=temperature_2m&hourly=temperature_2m&daily=temperature_2m_max,temperature_2m_min'
+    @url = 'https://api.open-meteo.com/v1/forecast?latitude=39.739235&longitude=-104.990250&current=temperature_2m&hourly=&daily=temperature_2m_max,temperature_2m_min&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&timezone=America%2FDenver'
     @uri = URI(@url)
     @response = Net::HTTP.get(@uri)
     @output = JSON.parse(@response)
