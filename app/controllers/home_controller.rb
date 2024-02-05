1c8b5632aafee7010a5872e28410d450217f8758
ali <alialdawoodi99@yahoo.com>
2024-02-05 10:01:43 -0700

    Added method in order to display 7 day forecast. Updated views according to this as well.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -26,21 +26,34 @@ class HomeController < ApplicationController
       @current_temp = @weather_data['current']['temperature_2m']
 
       daily_data = @weather_data['daily']
+      @time = daily_data['time']
+      @max_temp = daily_data['temperature_2m_max']
+      @min_temp = daily_data['temperature_2m_min']
 
-      daily_forecast = {
+     
+      @orders = []
+        @time.each_with_index do |time, index|
+          min_temp = @min_temp[index]
+          max_temp = @max_temp[index]
+          current_temp = ((min_temp + max_temp) / 2.0).round()
+          order = "Date: #{time}, Max Temperature:#{max_temp} Min Temperature:#{min_temp}, Current Temperature: #{current_temp}"
+          @orders << order
+    end
+     
+
+      @daily_forecast = {
         'time' => daily_data['time'],
         'temperature_2m-max' => daily_data['temperature_2m_max'].map {|day_data| day_data},
         'temperature_2m_min' => daily_data['temperature_2m_min'].map {|day_data| day_data}
 
-        
       }
-        [@current_temp, daily_forecast]
+        [@current_temp, @daily_forecast]
     end
 
-    @url = 'https://api.open-meteo.com/v1/forecast?latitude=39.739235&longitude=-104.990250&current=temperature_2m&hourly=&daily=temperature_2m_max,temperature_2m_min&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&timezone=America%2FDenver'
-    @uri = URI(@url)
-    @response = Net::HTTP.get(@uri)
-    @output2 = JSON.parse(@response)
+    # @url = 'https://api.open-meteo.com/v1/forecast?latitude=39.739235&longitude=-104.990250&current=temperature_2m&hourly=&daily=temperature_2m_max,temperature_2m_min&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&timezone=America%2FDenver'
+    # @uri = URI(@url)
+    # @response = Net::HTTP.get(@uri)
+    # @output2 = JSON.parse(@response)
 
     
     def main
